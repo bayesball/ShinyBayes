027acbffd9d3fd137ec656285eeca0c7c705aca6
Jim Albert <albertcb1@gmail.com>
2020-11-04 09:44:28 -0500

    modify file

--- inst/shiny-examples/TwoProportions/app.R
+++ inst/shiny-examples/TwoProportions/app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(ggplot2)
 library(ProbBayes)
 require(metR)
+require(gridExtra)
 
 # Define UI ----
 ui <- fluidPage(
@@ -10,47 +11,48 @@ ui <- fluidPage(
   tags$style(HTML("#big-heading{color: red;}")),
   fluidRow(
     column(4, wellPanel(
-#      h4(id="model-heading", "Model:"),
- #     tags$style(HTML("#model-heading{color: red;}")),
+     h4(id="model-heading", "Model:"),
+     tags$style(HTML("#model-heading{color: red;}")),
       h5("s1 is Binom(s1 + f1, p1)"),
       h5("s2 is Binom(s2 + f2, p2)"),
       h5("theta1 = logit(p1) - logit(p2)"),
       h5("theta2 = logit(p1) + logit(p2)"),
       h5("(theta1, theta2) is Uniform"),
-      h4(id="data-heading", "Enter Data:"),
+      h5('----------------------------'),
+      h4(id="data-heading", "Enter Successes and Failures:"),
       tags$style(HTML("#data-heading{color: red;}")),
-      sliderInput("s1", "s1 = # Successes in 1st Sample:", 
-                  min = 1, max = 20, 
-                  value = 5,
-                  step = 1),
-      sliderInput("f1", "f1 = # Failures in 1st Sample:", 
-                  min = 1, max = 20, 
-                  value = 10,
-                  step = 1),
-      sliderInput("s2", "s2 = # Successes in 2nd Sample:", 
-                  min = 1, max = 20, 
-                  value = 3,
-                  step = 1),
-      sliderInput("f2", "f2 = # Failures in 2nd Sample:", 
-                  min = 1, max = 20, 
-                  value = 8,
-                  step = 1),
-      sliderInput("iter", "# Simulations:", 
-                  min = 100, max = 5000, 
+      h5(id="data-heading1", "Sample 1:"),
+      fluidRow(
+        column(2,  HTML('<h5><b>s1:</b></h5>')),
+        column(4, numericInput("s1", "", min = 0, max = 100, value = 5)),
+        column(2, HTML('<h5><b>f1:</b></h5>')),
+        column(4, numericInput("f1", "", min = 0, max = 100, value = 5))
+      ),
+      h5(id="data-heading1", "Sample 2:"),
+      fluidRow(
+        column(2,  HTML('<h5><b>s2:</b></h5>')),
+        column(4, numericInput("s2", "", min = 0, max = 100, value = 5)),
+        column(2, HTML('<h5><b>f2:</b></h5>')),
+        column(4, numericInput("f2", "", min = 0, max = 100, value = 5))
+      ),
+      sliderInput("iter", "# Simulations:",
+                  min = 100, max = 5000,
                   value = 1000,
                   step = 20)
     )),
     column(8,
-           plotOutput("plot", height = "380px"),
-           h4(id="data-heading", "Data:"),
-           tags$style(HTML("#data-heading{color: red;}")),
-    #       h4("Data:"),
-           verbatimTextOutput("stats"),
-           h4(id="stats-heading", "Simulation Posterior Summaries:"),
-           tags$style(HTML("#stats-heading{color: red;}")),
-    
-    #       h4("Simulation Posterior Summaries:"),
-           verbatimTextOutput("stats2")
+           tabsetPanel(type = "tabs",
+                       tabPanel("Data",
+                                tableOutput("stats")),
+                       tabPanel("Contour",
+                                plotOutput("plot",
+                                           height = "400px")),
+                       tabPanel("Marginals",
+                                plotOutput("mplot",
+                                           height = "400px")),
+                       tabPanel("Summaries",
+                                tableOutput("stats2"))
+           )
     )
   )
 )
@@ -61,7 +63,7 @@ server <- function(input, output) {
     LOGF <- function(theta, ...) {
       if (is.matrix(theta) == TRUE) {
         val <- matrix(0, c(dim(theta)[1], 1))
-        for (j in 1:dim(theta)[1]){ 
+        for (j in 1:dim(theta)[1]){
           val[j] <- logf(theta[j, ], ...)
         }
       }
@@ -84,50 +86,50 @@ server <- function(input, output) {
                         breaks=BR,
                         size=1.5) +
       scale_fill_distiller(palette="Spectral") +
-      theme(text=element_text(size=18)) 
+      theme(text=element_text(size=18))
   }
   data <- reactive({
     logpost <- function(theta){
-      d <- c(input$s1, input$f1, 
+      d <- c(input$s1, input$f1,
              input$s2, input$f2)
       logctablepost(theta, d)
     }
     fit <- laplace(logpost, c(0, 0))
     mo <- fit$mode
     sds <- sqrt(diag(fit$var))
-    mdata <- matrix(c(input$s1, input$f1, 
-                      input$s2, input$f2), 
+    mdata <- matrix(c(input$s1, input$f1,
+                      input$s2, input$f2),
                     2, 2, byrow = TRUE)
     dimnames(mdata)[[2]] <- c("Successes", "Failures")
     dimnames(mdata)[[1]] <- c("Sample 1", "Sample 2")
-    summ <- data.frame(Parameter = 
+    summ <- data.frame(Parameter =
                          c("theta1", "theta2"),
                        Mean = mo,
                        Standard_Deviation = sds)
-    limits <- c(mo[1] - 5 * sds[1], 
+    limits <- c(mo[1] - 5 * sds[1],
                 mo[1] + 5 * sds[1],
                 mo[2] - 5 * sds[2],
                 mo[2] + 5 * sds[2])
-    
+
     sim_post <- simcontour(logctablepost, limits,
         c(input$s1, input$f1, input$s2, input$f2),
         input$iter)
-    new_mo <- as.character(round(c(mean(sim_post$x), 
+    new_mo <- as.character(round(c(mean(sim_post$x),
                 mean(sim_post$y)), 3))
-    new_sd <- as.character(round(c(sd(sim_post$x), 
+    new_sd <- as.character(round(c(sd(sim_post$x),
                 sd(sim_post$y)), 3))
-    new_cor <- as.character(round(cor(sim_post$x, 
+    new_cor <- as.character(round(cor(sim_post$x,
                       sim_post$y), 3))
-    prob <- as.character(round(mean(sim_post$x > 
+    prob <- as.character(round(mean(sim_post$x >
                         0), 3))
-    newsumm <- data.frame(Parameter = 
+    newsumm <- data.frame(Parameter =
                          c("theta1", "theta2",
                            "(theta1, theta2)",
                            "P(theta1 > 0)"),
               Mean = c(new_mo, "", prob),
             Stan_Dev = c(new_sd, "", ""),
             Correlation = c("", "", new_cor, ""))
-    
+
     sim_post <- data.frame(x = sim_post$x,
                            y = sim_post$y)
     gplot <- gcontour2(logpost, limits) +
@@ -135,7 +137,8 @@ server <- function(input, output) {
                  alpha = 0.5, color = "black")
     list(data = mdata,
          summary = newsumm,
-         plot = gplot)
+         plot = gplot,
+         sim_post = sim_post)
   })
   output$plot <- renderPlot({
     out <- data()
@@ -145,22 +148,39 @@ server <- function(input, output) {
     data()$plot +
       increasefont() +
       ggtitle("Posterior of (theta1, theta2)") +
-      theme(plot.title = element_text(colour = "blue", 
-          size = 24, 
+      theme(plot.title = element_text(colour = "blue",
+          size = 24,
          hjust = 0.5, vjust = 0.8, angle = 0)) +
       xlab("theta1 = Difference in Logits") +
       ylab("theta2 = Sum of Logits") +
       theme(
-        axis.title = element_text(color = "blue", 
+        axis.title = element_text(color = "blue",
                     size = 18))
   })
-  output$stats <- renderPrint({
-    data()$data
+  output$mplot <- renderPlot({
+    out <- data()
+    p1 <- ggplot(out$sim_post, aes(x)) +
+      geom_density(size = 2, color = "red") +
+      xlab("theta1") +
+      ggtitle("Difference in Logits") +
+      increasefont() + centertitle()
+    p2 <- ggplot(out$sim_post, aes(y)) +
+      geom_density(size = 2, color = "red") +
+      xlab("theta2") +
+      ggtitle("Sum of Logits") +
+      increasefont() + centertitle()
+    grid.arrange(p1, p2, ncol = 1)
   })
-  output$stats2 <- renderPrint({
+  output$stats <- renderTable({
+    as.data.frame(data()$data) -> df
+    df$Sample <- c("Sample 1", "Sample 2")
+    df$'Sample Size' <- df[, 1] + df[, 2]
+    df[, c(3, 1, 2, 4)]
+  })
+  output$stats2 <- renderTable({
     data()$summary
   })
 }
-  
+
 # Run the app ----
 shinyApp(ui = ui, server = server)
